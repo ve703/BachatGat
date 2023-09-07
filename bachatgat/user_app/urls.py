f61bb3007db99cbd0ec06623da26c2d559c4bd44
Vedant Akare <81575978+ve703@users.noreply.github.com>
2023-09-07 21:26:07 +0530

    Delete bachatgat directory

--- bachatgat/user_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.user_login, name='login'),
-    # path('login/', views.ProfileModelBackend, name='login'),
-    path('home/', views.home, name='home'),
-]
-
